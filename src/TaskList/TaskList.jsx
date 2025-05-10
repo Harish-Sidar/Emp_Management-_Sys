dfa7cce81eb4ebd4121b6a423177150f79cec3ba
hk265 <Nimcet9900@gmail.com>
2025-05-11 00:19:45 +0530

    4 commit

---
+++ src/TaskList/TaskList.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div className='h-[55%] flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10 bg-red-500'> 
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div> 
+    <div className='h-full flex-shrink-0 w-[300px] bg-yellow-400 rounded-xl'></div>
+
+
+    </div>
+  )
+}
+
+export default TaskList 
